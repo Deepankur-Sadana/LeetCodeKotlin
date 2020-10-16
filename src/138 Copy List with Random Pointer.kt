bab9cffcf4953858732983797a6a9adc6d46b16c
writetodeepankursadana@gmail.com <writetodeepankursadana@gmail.com>
2020-10-17 03:16:57 +0530

    138 Copy List with Random Pointer.kt
    Constant space

--- src/138 Copy List with Random Pointer.kt
+++ src/138 Copy List with Random Pointer.kt
@@ -26,6 +26,48 @@ class `138 Copy List with Random Pointer` {
         }
         return map[node]
     }
+
+    // Constant space
+    class ConstantSpace {
+
+
+        /**
+         *Runtime: 164 ms, faster than 99.11% of Kotlin online submissions for Copy List with Random Pointer.
+        Memory Usage: 38.5 MB, less than 5.36% of Kotlin online submissions for Copy List with Random Pointer.
+
+         */
+        fun copyRandomList(node: Node?): Node? {
+            if (node == null) return node
+            var curr = node
+            while (curr != null) {
+                val incoming = Node(curr.`val`)
+                val next = curr.next
+                incoming.next = next
+                curr.next = incoming
+                curr = curr.next?.next
+            }
+            curr = node
+
+            val res = node?.next
+            while (curr != null) {
+                val random = curr.random
+                curr.next?.random = random?.next
+                curr = curr.next?.next
+            }
+            curr = node
+
+            while (curr != null) {
+                val nextCurr = curr.next?.next
+                val copy = curr.next!!
+                curr.next = copy.next
+                copy.next = copy.next?.next
+                curr = nextCurr
+            }
+            return res
+        }
+
+    }
 }
+
 
 
