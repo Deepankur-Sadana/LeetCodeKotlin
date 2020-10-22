a3eaadb9cdd43da2d10bf6383da6a9b5e2d593ef
writetodeepankursadana@gmail.com <writetodeepankursadana@gmail.com>
2020-10-23 03:30:43 +0530

    1570 Dot Product of Two Sparse Vectors.kt  (//Math.min (m,n))

--- src/1570 Dot Product of Two Sparse Vectors.kt
+++ src/1570 Dot Product of Two Sparse Vectors.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.LinkedHashMap
 
 /**
  * Runtime: 808 ms, faster than 15.15% of Kotlin online submissions for Dot Product of Two Sparse Vectors.
@@ -43,4 +44,42 @@ class `1570 Dot Product of Two Sparse Vectors` {
         }
     }
 
+}
+
+/**
+ * Runtime: 728 ms, faster than 57.58% of Kotlin online submissions for Dot Product of Two Sparse Vectors.
+Memory Usage: 56.8 MB, less than 6.06% of Kotlin online submissions for Dot Product of Two Sparse Vectors.
+
+ */
+//Math.min (m,n)
+class SparseVector(nums: IntArray) {
+    private val positiveNum = LinkedHashMap<Int, Int>()// index, num
+
+    init {
+        nums.forEachIndexed { index, num -> if (num != 0) positiveNum[index] = (num) }
+    }
+
+    // Return the dotProduct of two sparse vectors
+    fun dotProduct(other: SparseVector): Int {
+        if (this.positiveNum.size == 0 || other.positiveNum.size == 0) return 0
+
+        val small: LinkedHashMap<Int, Int>
+        val big: LinkedHashMap<Int, Int>
+        if (other.positiveNum.size < this.positiveNum.size) {
+            small = other.positiveNum
+            big = this.positiveNum
+        } else {
+            big = other.positiveNum
+            small = this.positiveNum
+        }
+        var sum = 0
+        small.entries.forEach {
+            if (big.containsKey(it.key)) {
+                sum += it.value * big[it.key]!!
+            }
+
+
+        }
+        return sum
+    }
 }
