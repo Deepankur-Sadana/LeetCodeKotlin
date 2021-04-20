d3496e8fc05d9d3e10fa45ed07b74ef1b2336115
deepankur <deepankus@zeta.tech>
2021-04-20 20:47:21 +0530

    426 Convert Binary Search Tree to Sorted Doubly Linked List

--- src/426 Convert Binary Search Tree to Sorted Doubly Linked List.kt
+++ src/426 Convert Binary Search Tree to Sorted Doubly Linked List.kt
@@ -5,14 +5,34 @@ class `426 Convert Binary Search Tree to Sorted Doubly Linked List` {
         var right: Node? = null
     }
 
+    /**
+     * Runtime: 156 ms, faster than 80.00% of Kotlin online submissions for Convert Binary Search Tree to Sorted Doubly Linked List.
+    Memory Usage: 35 MB, less than 32.86% of Kotlin online submissions for Convert Binary Search Tree to Sorted Doubly Linked List.
+     */
     fun treeToDoublyList(root: Node?): Node? {
-TODO()
+        recurse(root)
+        firstPrintedElement?.left = latestPrintedElement
+        latestPrintedElement?.right = firstPrintedElement
+        return firstPrintedElement
+
     }
 
-    fun recurse(root: Node?) {
+    private var firstPrintedElement: Node? = null
+    private var latestPrintedElement: Node? = null
+
+    private fun recurse(root: Node?) {
         if (root == null) return
         recurse(root.left)
-        ///
-        recurse(root.right)
+
+        val nextNodeToRecurse = root.right
+        root.left = latestPrintedElement
+        latestPrintedElement?.right = root
+        println(root)
+        latestPrintedElement = root
+
+        if (firstPrintedElement == null)
+            firstPrintedElement = root
+
+        recurse(nextNodeToRecurse)
     }
 }
