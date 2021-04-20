4a0f531d06a6b6ef52c7e65551420ed24a57ef62
deepankur <deepankus@zeta.tech>
2021-04-20 19:25:32 +0530

    updating gitignore

---
+++ src/543 Diameter of Binary Tree.kt
@@ -0,0 +1,14 @@
+class `543 Diameter of Binary Tree` {
+
+    var max = 0
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+
+    }
+
+    fun recurse(root: TreeNode?): Int {
+        if (root == null) return 0
+
+
+    }
+
+}
