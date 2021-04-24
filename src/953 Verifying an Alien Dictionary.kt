bde44a37d125366a961f6e91f3defbcddfebd680
deepankur <deepankus@zeta.tech>
2021-04-25 03:22:24 +0530

    953 Verifying an Alien Dictionary

--- src/953 Verifying an Alien Dictionary.kt
+++ src/953 Verifying an Alien Dictionary.kt
@@ -1,20 +1,42 @@
 class `953 Verifying an Alien Dictionary` {
 
-    fun isAlienSorted(words: Array<String>, order: String): Boolean {
-        val map = HashMap<Char, Int>()
-        order.forEachIndexed { index, char ->
-            map[char] = index
-        }
+    /**
+     * Runtime: 172 ms, faster than 64.82% of Kotlin online submissions for Verifying an Alien Dictionary.
+    Memory Usage: 35 MB, less than 100.00% of Kotlin online submissions for Verifying an Alien Dictionary.
 
-        val currentOrder = ArrayList<Int>()
-        words[0].forEach { currentOrder.add(map[it]!!) }
-        for (i in 1 until words.size) {
-            val word = words[i]
-            word.forEachIndexed{ index, char ->
-//                map[char]>=currentOrder
+     */
+    val map = LinkedHashMap<Char, Int>()
+    fun isAlienSorted(words: Array<String>, order: String): Boolean {
+
+        order.toCharArray().forEachIndexed { index, element ->
+            map[element] = index
+        }
+        for (i in 0..words.size - 2) {
+            if (isSorted(words[i], words[i + 1]).not()) return false
+        }
+        return true
+    }
+
+    private fun isSorted(left: String, right: String): Boolean {
+        var a = 0
+        var b = 0
+
+        var found = false
+        while (a < left.length && b < right.length) {
+            if (map[left[a]]!! > map[right[b]]!!) {
+                return false
             }
 
+            if (map[left[a]]!! < map[right[b]]!!) {
+                return true
+            }
+
+
+            ++a
+            ++b
         }
+
+        if (left.length>right.length) return false
 
         return true
     }
